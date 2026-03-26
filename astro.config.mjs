1501fc5a35a49c1d3e3fc1eeb503e2a01351994b
Antonio <antperdev+github@gmail.com>
2026-03-26 13:15:35 +0100

    added grettings with preact and a rss.xml for feeds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    devToolbar: {
+    enabled: false
+  },
+  site: "https://boisterous-begonia-2e9726.netlify.app/",
+  integrations: [preact()]
+});
